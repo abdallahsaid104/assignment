674db7140792de4ed0411036cc41ee62fed60ac6
abdallah said <abdallah said>
2021-04-16 17:50:18 +0200

    new line

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+    char name [50];
+    int ID;
+    int day;
+    int month;
+    int year;
+    float last_year_score;
+};
+int main()
+{
+    printf("\n\n\n \t\t\t\t\t welcome to the students system \n\n\n");
+    return 0;
+}
