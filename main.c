4dcdd191b13152be3213256f822779940e11d3bc
sohilaakram <sohila.akram.3@gmail.com>
2021-04-18 02:51:29 +0200

    End Function

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ struct Student {
 }*head,*tail;   //Global pointer of type student that will point to the first node
 
 
+
 //insert_beginning Function should take student details as argument and create new node with those details at the beginning
 void insert_beginning (char* name,int ID , int day , int month , int year , int last_year_score)
 {
@@ -56,17 +57,25 @@ void insert_end (char* name,int ID , int day , int month , int year , float last
 	student->month=month;
 	student->year=year;
 	student->next = NULL;
+	void tailptr() {
+	tail=student;
+	while (tail->next!=NULL) {
 
-	if(head==NULL){
+        tail=student->next;
+	}
+
+	}
+    tailptr();
+	if(tail==NULL){
 		// if head is NULL
 		// set student as the new head
-		head = student;
+		tail = student;
 	}
 	else{
 		// if list is not empty
 		// insert student in end
-		student->next = NULL;
-		//tail= student;
+		student->next = tail;
+		tail= student;
 	}
 
 
@@ -107,6 +116,7 @@ void insert_mid (char* name,int ID , int day , int month , int year , int last_y
 void display()
 {
     struct Student * temp = head;
+    struct Student * tem = tail;
     while(temp!=NULL){
 
         printf("Name: %s\n", temp->name);
@@ -115,6 +125,12 @@ void display()
         printf("Birth Date: %d/%d/%d\n\n",temp->day,temp->month,temp->year);
         temp = temp->next;
 
+        printf("Name: %s\n", tem->name);
+        printf("ID: %d\n", tem->ID);
+        printf("Percentage: %d \n", tem->last_year_score);
+        printf("Birth Date: %d/%d/%d\n\n",tem->day,tem->month,tem->year);
+        tem = tem->next;
+
     }
 }
 
@@ -122,19 +138,14 @@ int main()
 {
     printf("\n\n\n \t\t\t\t\t Welcome to The Student Management System \n\n\n");
 
-    char name [50];
-    int ID;
-    int day;
-    int month;
-    int year;
-    int last_year_score;
-    head= NULL;
+
     insert_beginning("Sohila",1000,3,3,2001,83);
     insert_beginning("Abdallah",1001,10,4,2000,84);
     insert_beginning("Yara",1003,6,4,2006,80);
     insert_beginning("Menna",1004,19,11,2002,70);
     insert_beginning("Abdelaziz",1005,17,10,2000,88);
     insert_mid("Salma",1002,15,6,2000,75,4);                 //Bug at Position 1
+    insert_end("3abbas",999,2,8,1980,67);
     display();
     return 0;
 }
