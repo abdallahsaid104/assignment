2f89db02ff55cf952bcb225c3618505c4655d6d4
sohilaakram <sohila.akram.3@gmail.com>
2021-04-18 05:45:14 +0200

    Student2

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ struct Student {
 //struct student is a Self Referential structure that have one or more
 //pointers which point to the same type of structure, as their member.
 
-}*head,*tail;   //Global pointer of type student that will point to the first node
+}*head,*tail;   //Global pointers of type student that will point to the first node
 
 
 
@@ -57,27 +57,15 @@ void insert_end (char* name,int ID , int day , int month , int year , float last
 	student->month=month;
 	student->year=year;
 	student->next = NULL;
-	void tailptr() {
-	tail=student;
-	while (tail->next!=NULL) {
+    tail=head;
+    while(tail != NULL && tail->next != NULL)
+    {
+        tail=tail->next;
+    }
 
-        tail=student->next;
-	}
-
-	}
-    tailptr();
-	if(tail==NULL){
-		// if head is NULL
-		// set student as the new head
-		tail = student;
-	}
-	else{
-		// if list is not empty
-		// insert student in end
-		student->next = tail;
-		tail= student;
-	}
-
+        student->next = NULL;
+        tail->next = student;
+        tail=student;
 
 }
 
@@ -117,35 +105,42 @@ void display()
 {
     struct Student * temp = head;
     struct Student * tem = tail;
-    while(temp!=NULL){
+    while(temp!=NULL ){
 
         printf("Name: %s\n", temp->name);
         printf("ID: %d\n", temp->ID);
         printf("Percentage: %d \n", temp->last_year_score);
         printf("Birth Date: %d/%d/%d\n\n",temp->day,temp->month,temp->year);
         temp = temp->next;
-
+    }
+    while (tem!=NULL)
+    {
         printf("Name: %s\n", tem->name);
         printf("ID: %d\n", tem->ID);
         printf("Percentage: %d \n", tem->last_year_score);
         printf("Birth Date: %d/%d/%d\n\n",tem->day,tem->month,tem->year);
-        tem = tem->next;
+        tem= tem->next;
 
     }
-}
+
+
+
+    }
+
 
 int main()
 {
-    printf("\n\n\n \t\t\t\t\t Welcome to The Student Management System \n\n\n");
 
+    printf("\n\n\n \t\t\t\t\t Welcome to The Student Management System \n\n\n");
 
     insert_beginning("Sohila",1000,3,3,2001,83);
     insert_beginning("Abdallah",1001,10,4,2000,84);
+    insert_end("3abbas",999,2,8,1980,67);                     //Bug (Inserted twice )
     insert_beginning("Yara",1003,6,4,2006,80);
     insert_beginning("Menna",1004,19,11,2002,70);
     insert_beginning("Abdelaziz",1005,17,10,2000,88);
     insert_mid("Salma",1002,15,6,2000,75,4);                 //Bug at Position 1
-    insert_end("3abbas",999,2,8,1980,67);
+    insert_end("3absame3",998,6,12,1986,69);                    ////Bug (Inserted twice )
     display();
     return 0;
 }
