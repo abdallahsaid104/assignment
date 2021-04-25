a54d8333fde42c842b67c503f67fe233fcb28069
sohilaakram <sohila.akram.3@gmail.com>
2021-04-26 01:35:29 +0200

    Corner Cases

--- main.c
+++ main.c
@@ -15,7 +15,18 @@ struct Student {
 
 }*head,*tail;   //Global pointers of type student that will point to the first node
 
+int  count_nodes()
+{
 
+int count=0;
+struct Student* temp = head;
+while (temp!=NULL)
+{
+    count++;
+    temp=temp->next;
+}
+return count;
+}
 
 //insert_beginning Function should take student details as argument and create new node with those details at the beginning
 void insert_beginning (char* name,int ID , int day , int month , int year , int last_year_score)
@@ -63,7 +74,7 @@ void insert_end (char* name,int ID , int day , int month , int year , float last
         tail=tail->next;
     }
 
-        //student->next = NULL;
+        student->next = NULL;
         tail->next = student;
         tail=student;
 
@@ -90,14 +101,23 @@ void insert_mid (char* name,int ID , int day , int month , int year , int last_y
 	else{
 		// if list is not empty
 		// insert student in beginning of head
-		temp=head;
 
+        int length=count_nodes();
 		if (position==1)
         {
             insert_beginning( name, ID , day , month , year , last_year_score);
         }
 
-
+        else if (position==length)
+        {
+            insert_end( name, ID , day , month , year , last_year_score);
+        }
+       else if (position>length+1 || position<1)
+       {
+           printf("Error! \nThe length of the list is %d \nPlease choose an integer appropriate position!\n\n",length);
+       }
+        else {
+        temp=head;
 		for (int i=0;i<position-2;i++)
         {
             temp=temp->next;
@@ -106,7 +126,7 @@ void insert_mid (char* name,int ID , int day , int month , int year , int last_y
             temp->next=student;
 	}
 
-
+	}
 }
 void display()
 {
@@ -119,10 +139,6 @@ void display()
         printf("Birth Date: %d/%d/%d\n\n",temp->day,temp->month,temp->year);
         temp = temp->next;
     }
-
-
-
-
     }
 
 
@@ -134,11 +150,12 @@ int main()
     insert_beginning("Sohila",1000,3,3,2001,83);
     insert_beginning("Abdallah",1001,10,4,2000,84);
     insert_end("3abbas",999,2,8,1980,67);
-    insert_beginning("Yara",1003,6,4,2006,80);
+    insert_beginning("Yara",1003,6,4,2006,95);
     insert_beginning("Menna",1004,19,11,2002,70);
     insert_beginning("Abdelaziz",1005,17,10,2000,88);
-    insert_mid("Salma",1002,15,6,2000,75,4);
+    insert_mid("Salma",1002,15,6,2000,75,7);
     insert_end("3absame3",998,6,12,1986,69);
+    insert_mid("Sarah",997,12,4,1957,65,12);
     display();
     return 0;
 }
